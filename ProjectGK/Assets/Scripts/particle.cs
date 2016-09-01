05f46218441b2744f79abd87e7777d6555fe4ffd
Mateusz <mateusz.pindel@onet.pl>
2016-09-02 00:00:04 +0200

    Stworzenie PAR - efekt cząsteczkowy, w formie dymu

--- ProjectGK/Assets/Scripts/particle.cs
+++ ProjectGK/Assets/Scripts/particle.cs
@@ -2,51 +2,72 @@
 
 public class particle : MonoBehaviour
 {
-	static Random random = new Random();
-
-	Vector2 acceleration = new Vector2(0.001f, 0.001f);
-	Vector2 velocity = new Vector2(Random.value, Random.value);
-	int lifeTime = 24;
-	Vector2 position;
+	public static int lifeTime;
+	Vector3 velocity;
+	Vector3 acceleration;
+	Vector3 position;
+	float scale;
+	Color color;
 
 	// Use this for initialization
 	void Start()
 	{
-		lifeTime = 1000;
+		transform.position += randomVector ();
+		position = transform.position;
+		acceleration = randomVector () / 20000;
+		velocity = randomVector () / 20000;
+
 		particleSystem ps = (particleSystem)FindObjectOfType(typeof(particleSystem));
-		position = ps.transform.position;
-		velocity = new Vector2(Random.Range(-1f,1f)/8, Random.Range(-1f,1f)/8);
+		lifeTime = 200;
+
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
 		velocity += acceleration;
-		float newX = transform.localScale.x - 0.02f;
-		//GetComponent<Renderer>().material.color = Color.red;
-		if (newX > 0)
-		{
-			transform.localScale = new Vector3(newX, newX, newX);
-		}
 		position += velocity;
-		lifeTime--;
-
 		transform.position = position;
+		transform.Rotate (0.5f, 0.5f, 0.5f);
 
-		if (isDead())
-		{
-			Destroy(gameObject);
-		}
+	//	if (lifeTime < (lifeTime / 10)) {
+	//		scale = transform.lossyScale.x - 0.1f;
+	//		if (scale > 0)
+	//			transform.localScale = new Vector3 (scale, scale, scale);
+	//	}
 
-	}
+	//	color = transform.GetComponent<SpriteRenderer>().color;
+	//	color = new Color (color.r, color.g, color.b, color.a - 3.0f);
+	//	transform.GetComponent<SpriteRenderer> ().color = color;
 
-	public particle(Vector2 position)
-	{
-		this.position = position;
+	//	transform.GetComponent<Shader> ().maximumLOD (UIVertex.simpleVert.color.a);
+
+		lifeTime--;
+	//	Color color = transform.GetComponent<Shader> ().color;
+
+
+	//	if (isDead ()) 
+		//Destroy (transform.gameObject);
+	//		Destroy(gameObject);
 	}
 
 	public bool isDead()
 	{
 		return lifeTime < 0;
 	}
+
+	public Vector3 randomVector(){
+		float x, y, z;
+		Vector3 movement;
+
+		x = Random.Range(-0.25f, 0.25f);
+		y = Random.Range(0.0f, 1.0f);
+		z = Random.Range(-0.25f, 0.25f);
+
+		movement = new Vector3 (x, y, z);
+
+		return movement;
+	}
+
+
 }
--- ProjectGK/Assets/Scripts/particleSystem.cs
+++ ProjectGK/Assets/Scripts/particleSystem.cs
@@ -3,27 +3,27 @@ using System.Collections;
 
 public class particleSystem : MonoBehaviour
 {
-	public static bool boom = false;
 	public GameObject obj;
 	ArrayList particles = new ArrayList();
-	static int limit;
-	// Use this for initialization
+	int limit;
+
 	void Start()
 	{
-		limit = 1000;
+		limit = particle.lifeTime;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (limit>0)
-		{
-			for(int i = 0; i < 50; i++)
-			{
-				particles.Add(Instantiate(obj, transform.position, Quaternion.identity));
-				limit--;
-
-			}
-		}
+		particles.Add ( Instantiate (obj, transform.position, Quaternion.identity));
+			
+		if ( (particles.Count/2) == limit ) {
+			Destroy ((GameObject) particles [0]);
+			particles.RemoveAt (0);
+			Destroy ((GameObject) particles [1]);
+			particles.RemoveAt (1);
+			Destroy ((GameObject) particles [2]);
+			particles.RemoveAt (2);
+		}		
 	}
 }
