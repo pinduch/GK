e764613044ca5394f216c4699c1af480416e681f
Mateusz <mateusz.pindel@onet.pl>
2016-09-06 00:29:20 +0200

    Stworzenie animacji marszu.

--- ProjectGK/Assets/Scripts/MoveAnimation.cs
+++ ProjectGK/Assets/Scripts/MoveAnimation.cs
@@ -28,41 +28,86 @@ public class MoveAnimation : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-
-
-		if (legForward) {
-			if (counter < 100) {
-				leftUpLeg.Rotate (0f, 0f, 0.4f);
-				leftLeg.Rotate (0f, 0f, -0.4f);
-			} else {
-				if (counter < 150) {
-					leftLeg.Rotate (0f, 0f, 0.5f);
-					rightUpLeg.Rotate (0f, 0f, -0.5f);
-					rightLeg.Rotate (0f, 0f, -0.5f);
-					skelet.position += skelet.forward * 0.015f;
-				} else {
-					legForward = false;
-				}
-			}
-			counter++;
-		} else {
-			if (counter > 100) {
-				leftLeg.Rotate (0f, 0f, -0.5f);
-				rightUpLeg.Rotate (0f, 0f, 0.5f);
-				rightLeg.Rotate (0f, 0f, 0.5f);
-				skelet.position += skelet.forward * 0.015f;
-			} else {
-				if (counter > 0) {
-					leftUpLeg.Rotate (0f, 0f, -0.4f);
-					leftLeg.Rotate (0f, 0f, 0.4f);
-				} else {
-					legForward = true;
-				}
-			}
-			counter--;
+		if (counter < 100) {
+			leftUpLeg.Rotate (0f, 0f, -0.2f); 	// lewa do tyłu
+			leftLeg.Rotate (0f, 0f, -0.4f);		// lewy piszczel w tył
+			rightUpLeg.Rotate (0f, 0f, 0.2f);	// prawa w przód  --  to jest pozycja startowa do marszu
+			leftArm.Rotate(0f,0f, 0.175f);
+			leftForeArm.Rotate (0f, 0f, 0.2f);
+			rightArm.Rotate(0f, 0f, -0.175f);
+		} else if (counter < 200) {
+			leftUpLeg.Rotate (0f, 0f, 0.4f);   // lewa przód
+			rightUpLeg.Rotate (0f, 0f, -0.4f); // prawa w tył
+			leftArm.Rotate(0f,0f,-0.3f);
+			leftForeArm.Rotate (0f, 0f, -0.2f);
+			rightArm.Rotate(0f, 0f, 0.3f);
+			rightForeArm.Rotate (0f, 0f, 0.2f);
+			skelet.position += skelet.forward * 0.018f;
+		} else if (counter < 250) {
+			leftLeg.Rotate (0f, 0f, 0.8f);  	// wyprostuj lewy piszczel
+			rightLeg.Rotate (0f, 0f, -0.8f);	// zegnij prawy piszczel
+			leftArm.Rotate(0f,0f,-0.1f);
+			rightArm.Rotate(0f, 0f, 0.1f);
+			skelet.position += skelet.forward * 0.011f;
+		} else if (counter < 350) {
+			rightUpLeg.Rotate (0f, 0f, 0.4f); 	//
+			leftUpLeg.Rotate (0f, 0f, -0.4f);
+			leftArm.Rotate(0f,0f, 0.3f);
+			leftForeArm.Rotate (0f, 0f, 0.2f);
+			rightArm.Rotate(0f, 0f,-0.3f);
+			rightForeArm.Rotate (0f, 0f, -0.2f);
+			skelet.position += skelet.forward * 0.018f;
+		} else if (counter < 400) {
+			rightLeg.Rotate (0f, 0f, 0.8f);
+			leftLeg.Rotate (0f, 0f, -0.8f);
+			leftArm.Rotate(0f,0f, 0.1f);
+			rightArm.Rotate(0f, 0f, -0.1f);
+			skelet.position += skelet.forward * 0.011f;
 		}
+		counter++;
 
+		if (counter > 400)
+			counter = 100;
+
+		/*			if (counter < 100) {
+			leftUpLeg.Rotate (0f, 0f, -0.2f); 	// lewa do tyłu
+			leftLeg.Rotate (0f, 0f, -0.2f);		// lewy piszczel w tył
+			rightUpLeg.Rotate (0f, 0f, 0.2f);	// prawa w przód  --  to jest pozycja startowa do marszu
+			leftArm.Rotate(0f,0f, 0.175f);
+			leftForeArm.Rotate (0f, 0f, 0.2f);
+			rightArm.Rotate(0f, 0f, -0.175f);
+		} else if (counter < 200) {
+			leftUpLeg.Rotate (0f, 0f, 0.4f);   // lewa przód
+			rightUpLeg.Rotate (0f, 0f, -0.4f); // prawa w tył
+			leftArm.Rotate(0f,0f,-0.3f);
+			leftForeArm.Rotate (0f, 0f, -0.2f);
+			rightArm.Rotate(0f, 0f, 0.3f);
+			rightForeArm.Rotate (0f, 0f, 0.2f);
+			skelet.position += skelet.forward * 0.018f;
+		} else if (counter < 250) {
+			leftLeg.Rotate (0f, 0f, 0.4f);  	// wyprostuj lewy piszczel
+			rightLeg.Rotate (0f, 0f, -0.4f);	// zegnij prawy piszczel
+			leftArm.Rotate(0f,0f,-0.1f);
+			rightArm.Rotate(0f, 0f, 0.1f);
+			skelet.position += skelet.forward * 0.011f;
+		} else if (counter < 350) {
+			rightUpLeg.Rotate (0f, 0f, 0.4f); 	//
+			leftUpLeg.Rotate (0f, 0f, -0.4f);
+			leftArm.Rotate(0f,0f, 0.3f);
+			leftForeArm.Rotate (0f, 0f, 0.2f);
+			rightArm.Rotate(0f, 0f,-0.3f);
+			rightForeArm.Rotate (0f, 0f, -0.2f);
+			skelet.position += skelet.forward * 0.018f;
+		} else if (counter < 400) {
+			rightLeg.Rotate (0f, 0f, 0.4f);
+			leftLeg.Rotate (0f, 0f, -0.4f);
+			leftArm.Rotate(0f,0f, 0.1f);
+			rightArm.Rotate(0f, 0f, -0.1f);
+			skelet.position += skelet.forward * 0.011f;
+		}
+		*/
 	}
+		
 
 
 	public void startWalk(){
